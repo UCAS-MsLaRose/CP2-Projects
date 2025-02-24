3d1b8840f60673c06f398dc9661ca102bf113fc5
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-24 10:33:13 -0700

    multi page notes 2nd

---
+++ Notes_2nd/multi_page/calcs.py
@@ -0,0 +1,19 @@
+# Vienna LaRose, basic math equations
+
+def add(num_one, num_two):
+    return num_one+num_two
+
+def sub(num_one, num_two):
+    return num_one-num_two
+
+def div(num_one, num_two):
+    return num_one/num_two
+
+def mult(num_one, num_two):
+    return num_one*num_two
+
+if __name__ == "main":
+    add()
+    sub()
+    div()
+    mult()
---
+++ Notes_2nd/multi_page/main.py
@@ -0,0 +1,9 @@
+from calcs import add as x, sub as q
+
+print(x(4,6))
+print(q(4,6))
+
+def info():
+    return "Andrew", 35, "orange"
+
+name, age, color = info()
